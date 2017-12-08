a96ce9e65862c2620f57ae4875e0d5f545f6cd52
Hidde Fokkema <Hidde@wcw-stud-145-109-29-0.wireless.uva.nl>
2017-12-08 12:55:44 +0100

    Rule generator

---
+++ Rule_generator.py
@@ -0,0 +1,22 @@
+import math
+import numpy as np
+from collections import defaultdict, Counter
+from dataset import get_clean_data
+import fuzzy_functions as ff
+
+
+
+dataset = get_clean_data()
+print(dataset)
+
+
+
+def Rule_gen(input_data, output_data, Variables):
+	#Generates a rule for each input and output pair
+	rule = ff.Rule(1, input_data, "and",  output_data )
+	return rule,
+
+def rule_degree(rule, input_data, output_data):
+	n = len(input_data)
+	degree = 0 
+	for i in range(n):
