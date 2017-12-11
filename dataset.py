2a720048c3859766d9104ac758d98bbec1789a86
tomotten <tom_otten@live.nl>
2017-12-11 10:58:49 +0100

    moved some new functions to other file

--- dataset.py
+++ dataset.py
@@ -40,6 +40,40 @@ def count_occurence(data_column):
         res[unique[i]] = counts[i]
     return res
 
+
+
+def validation_split(data, ratio):
+    return data[0:math.floor(len(data)*ratio)], data[math.floor(len(data)*ratio):len(data)]
+
+
+def x_y_split(data):
+    res = []
+    for i in range(len(data)):
+        if data[i][len(data[0])-1] == '0':
+            res.append(False)
+        else:
+            res.append(True)
+    return data[:,0:len(data[0])-1], np.array(res)
+
+
+def disc_num_split(data, thres=20):
+    data_int_i = []
+    data_float_i = []
+    u = unique_vals(data)
+    for i in range(len(u)):
+        if u[i] < thres: # int
+            data_int_i.append(i)
+        else: #float
+            data_float_i.append(i)
+    data_int = []
+    for i in data_int_i:
+        data_int.append(data[:,i].tolist())
+    data_float = []
+    for i in data_float_i:
+        data_float.append(data[:,i].tolist())
+    return np.array(data_int).T, np.array(data_float).T
+
+
 ''' get a 2d np array with on each row a patient'''
 def get_clean_data():
     data_c = load_data("processed.cleveland.data")
